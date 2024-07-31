c307dfa204277616154eacbddb3ebd1f7938707c
UmerAsad2077 <[umerasad2020@gmail.com]>
2024-07-31 21:58:52 +0500

    Add styles for mobile design
    Added styles for mobile design in the styles.css file, also modified tailwind config file to include a custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      "clr-primary-600": "#1f2937",
+    },
     extend: {},
   },
   plugins: [],
